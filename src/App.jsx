3f89a328968af6495f13dca93db03f4b0ef8b87d
Alagham <hamzahalagbe27@gmail.com>
2024-11-26 17:32:24 +0100

    PromiseCard

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/create-card" element={<CardCreation />} />
+          
         </Routes>
       </Router>
     </>
